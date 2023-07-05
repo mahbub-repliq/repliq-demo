7e6ddb005d783934f6fefba4c1169bb9cf709b4d
mahbubnoyon506 <mahbubnoyon506@gmail.com>
2023-07-05 18:50:55 +0600

    alignment fixed in profile page

--- app/dashboard/profile/page.jsx
+++ app/dashboard/profile/page.jsx
@@ -12,48 +12,51 @@ import {
 import LargeButton from "@/Commons/LargeButton";
 import { MdOutlineClose } from "react-icons/md";
 import UpdateProfile from "./UpdateProfile";
-import dummyImage from '@/assets/images/avatar.jpg'
+import dummyImage from "@/assets/images/avatar.jpg";
 
 function Profile() {
   const [openProfile, setOpenProfile] = useState(false);
   return (
     <div>
-      <div className={`relative ${openProfile ? 'opacity-20 ' : ''}`}>
+      <div className={`relative ${openProfile ? "opacity-20 " : ""}`}>
         <PageTitle>Profile</PageTitle>
-        <div className="flex justify-center">
-          <BsPersonCircle size={100} />
-        </div>
+
         <div className="flex justify-center items-center">
-          <ul className="w-[40rem]">
-            <li className="flex items-center gap-4 pb-2">
-              <div className="w-12 h-12 bg-accent flex justify-center items-center rounded-full">
-                <BsFillPersonFill className="inline" size={25} />
+          <div className="w-[35rem]">
+            <div className="flex justify-start">
+              <BsPersonCircle size={150} />
+            </div>
+            <ul className=" mt-5">
+              <li className="flex items-center gap-4 pb-2">
+                <div className="w-12 h-12 bg-accent flex justify-center items-center rounded-full">
+                  <BsFillPersonFill className="inline" size={25} />
+                </div>
+                <p>Mahbub Noyon</p>
+              </li>
+              <li className="flex items-center gap-4 pb-2">
+                <div className="w-12 h-12 bg-accent flex justify-center items-center rounded-full">
+                  <HiIdentification className="inline" size={25} />
+                </div>
+                <p>Software Developer</p>
+              </li>
+              <li className="flex items-center gap-4 pb-2">
+                <div className="w-12 h-12 bg-accent flex justify-center items-center rounded-full">
+                  <BsFillEnvelopeFill className="inline" size={25} />
+                </div>
+                <p>mahbubul.alam@repliq.dev</p>
+              </li>
+              <li className="flex items-center gap-4 pb-2">
+                <div className="w-12 h-12 bg-accent flex justify-center items-center rounded-full">
+                  <AiFillPhone className="inline" size={25} />
+                </div>
+                <p>01687874697</p>
+              </li>
+            </ul>
+            <div className="flex justify-start items-center mt-4">
+              <div onClick={() => setOpenProfile(!openProfile)}>
+                <LargeButton>Update Profile</LargeButton>
               </div>
-              <p>Mahbub Noyon</p>
-            </li>
-            <li className="flex items-center gap-4 pb-2">
-              <div className="w-12 h-12 bg-accent flex justify-center items-center rounded-full">
-                <HiIdentification className="inline" size={25} />
-              </div>
-              <p>Software Developer</p>
-            </li>
-            <li className="flex items-center gap-4 pb-2">
-              <div className="w-12 h-12 bg-accent flex justify-center items-center rounded-full">
-                <BsFillEnvelopeFill className="inline" size={25} />
-              </div>
-              <p>mahbubul.alam@repliq.dev</p>
-            </li>
-            <li className="flex items-center gap-4 pb-2">
-              <div className="w-12 h-12 bg-accent flex justify-center items-center rounded-full">
-                <AiFillPhone className="inline" size={25} />
-              </div>
-              <p>01687874697</p>
-            </li>
-          </ul>
-        </div>
-        <div className="flex justify-center items-center mt-4">
-          <div onClick={() => setOpenProfile(!openProfile)}>
-            <LargeButton>Update Profile</LargeButton>
+            </div>
           </div>
         </div>
       </div>
